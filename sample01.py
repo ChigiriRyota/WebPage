03337956ce069b59ced17a183338956ffbe191a6
RyotaChigiri <s2022059@stu.musashino-u.ac.jp>
2022-09-28 14:00:56 +0900

    cgiファイルを作成

---
+++ sample01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.6
+import cgi
+import cgitb
+cgitb.enable()
+print("Content-Type: text/html")
+print()
+print(’<html><head></head>’)
+print(’<body>’)
+print(’<p>Hello, world.</p>’)
+print(’</body></html>’)
